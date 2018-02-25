237e14922fcf74d41589b97835889687d3380a23
Donatas Bražionis <donatas@quatrodev.com>
2018-02-25 16:29:50 +0200

    Added models.

---
+++ Models/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace dotnetcore_webapi_and_ravendb.Models.Dtos
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetcore_webapi_and_ravendb.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
