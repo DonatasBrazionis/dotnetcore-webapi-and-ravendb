3de75c4f005eba21b1f64fc64a966532f9751f67
Donatas Bražionis <donatas@quatrodev.com>
2018-02-25 15:42:13 +0200

    Added CORS.

--- Startup.cs
+++ Startup.cs
@@ -23,12 +23,21 @@ namespace dotnetcore_webapi_and_ravendb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowCredentials();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
