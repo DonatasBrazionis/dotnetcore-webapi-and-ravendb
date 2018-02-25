529f4d7e97b2eb36eb1ee7343ee10ff62311ff1c
Donatas Bražionis <donatas@quatrodev.com>
2018-02-25 16:49:09 +0200

    Created new Users controller.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_webapi_and_ravendb.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UsersController : Controller
+    {
+
+
+    }
+}
