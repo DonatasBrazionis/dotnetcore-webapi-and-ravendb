e072435fdda346fbe0dcad01a0478b7439ae1882
Donatas Bražionis <4donatas4@gmail.com>
2018-03-15 21:07:17 +0200

    Updated `User` model.

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,22 @@ namespace dotnetcore_webapi_and_ravendb.Models
     public class User
     {
         public string Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public UserType Type { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? DateModified { get; set; }
+
+        // DateTime in UTC when ban ends, any 
+        // time in the past is considered not banned.
+        // If null - not banned.
+        public DateTime? DateBanEndsUtc { get; set; }
+        public string BanDescription { get; set; }
+        public string BannedBy { get; set; }
     }
 }
