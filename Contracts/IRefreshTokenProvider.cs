6b39c8607f8f00ba9ae99cbd002036d46dfb83b8
Donatas Bražionis <4donatas4@gmail.com>
2018-03-15 23:14:58 +0200

    Added `CreateAsync` into `RefreshTokenProvider`.

--- Contracts/IRefreshTokenProvider.cs
+++ Contracts/IRefreshTokenProvider.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
 namespace dotnetcore_webapi_and_ravendb.Contracts
 {
     public interface IRefreshTokenProvider
     {
         string GenerateId(string refreshToken);
+        Task CreateAsync(string userId, string refreshToken, string remoteIpAddress, string userAgent, TimeSpan refreshTokenLifetime);
     }
 }
--- Providers/RefreshTokenProvider.cs
+++ Providers/RefreshTokenProvider.cs
@@ -1,13 +1,18 @@
+using System;
+using System.Threading.Tasks;
 using dotnetcore_webapi_and_ravendb.Contracts;
+using dotnetcore_webapi_and_ravendb.Models;
 
 namespace dotnetcore_webapi_and_ravendb.Providers
 {
     public class RefreshTokenProvider : IRefreshTokenProvider
     {
-        public RefreshTokenProvider(IPasswordHasherProvider passwordHasherProvider)
+        public RefreshTokenProvider(IRavenDatabaseProvider ravenDatabaseProvider, IPasswordHasherProvider passwordHasherProvider)
         {
+            RavenDatabaseProvider = ravenDatabaseProvider;
             PasswordHasherProvider = passwordHasherProvider;
         }
+        protected IRavenDatabaseProvider RavenDatabaseProvider { get; set; }
         protected IPasswordHasherProvider PasswordHasherProvider { get; set; }
 
         public string GenerateId(string refreshToken)
@@ -15,5 +20,22 @@ namespace dotnetcore_webapi_and_ravendb.Providers
             var hashedToken = PasswordHasherProvider.GetMD5(refreshToken);
             return $"refresh-token/{hashedToken}";
         }
+
+        public async Task CreateAsync(string userId, string refreshToken, string remoteIpAddress, string userAgent, TimeSpan refreshTokenLifetime)
+        {
+            var id = GenerateId(refreshToken);
+            var entity = new RefreshTokenDetails
+            {
+                Id = id,
+                UserId = userId,
+                RefreshToken = refreshToken,
+                DateCreated = DateTime.UtcNow,
+                IpAddress = remoteIpAddress,
+                UserAgent = userAgent,
+                DateExpires = DateTime.UtcNow + refreshTokenLifetime
+            };
+            await RavenDatabaseProvider.CreateEntity(entity);
+        }
+
     }
 }
